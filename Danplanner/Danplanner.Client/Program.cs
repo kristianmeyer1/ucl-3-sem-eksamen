b13dc91ff576a7ad868ed77c6ea618df4868d5f0
Anders Bøgehøj Iversen <andersbiversen35@gmail.com>
2025-11-12 12:47:41 +0100

    Yes sir

--- Danplanner/Danplanner.Client/Program.cs
+++ Danplanner/Danplanner.Client/Program.cs
@@ -3,13 +3,14 @@ using Danplanner.Application.Services;
 using Danplanner.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Danplanner.Persistence.Repositories;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddScoped<IAdminRepository>();
+builder.Services.AddScoped<IAdminRepository,AdminRepository>();
 builder.Services.AddScoped<AdminService>();
 
 await builder.Build().RunAsync();
