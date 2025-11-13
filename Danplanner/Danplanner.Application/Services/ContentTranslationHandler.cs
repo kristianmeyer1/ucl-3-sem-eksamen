1b77ed4c0622ba6928af1ed029d983bf55d4b87b
kristian meyer <183391596+mever91@users.noreply.github.com>
2025-11-13 11:53:11 +0100

    added api (da,en,de,nl)

---
+++ Danplanner/Danplanner.Application/Services/ContentTranslationHandler.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Danplanner.Domain.Interfaces;
+
+namespace Danplanner.Application.Services
+{
+    public class ContentTranslationHandler
+    {
+        private readonly ITranslationService _translationService;
+
+        public ContentTranslationHandler(ITranslationService translationService)
+        {
+            _translationService = translationService;
+        }
+
+        public async Task<string> TranslationContentAsync(string text, string languageCode)
+        {
+            return await _translationService.TranslateTextAsync(text, languageCode);
+        }
+    }
+}
