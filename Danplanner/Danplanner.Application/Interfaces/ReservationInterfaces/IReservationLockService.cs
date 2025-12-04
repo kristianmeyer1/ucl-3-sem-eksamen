f4d978672dc74ef1853c50d18194afdd1e365139
Anders Bøgehøj Iversen <andersbiversen35@gmail.com>
2025-12-04 13:47:15 +0100

    created a token for concurrency

---
+++ Danplanner/Danplanner.Application/Interfaces/ReservationInterfaces/IReservationLockService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danplanner.Application.Interfaces.ReservationInterfaces
+{
+    public interface IReservationLockService
+    {
+        Task<(bool Success, string Token, DateTime ExpiresAt, string Message)> TryLockAsync(string key, TimeSpan ttl, string owner);
+        Task<bool> ValidateTokenAsync(string key, string token);
+        Task ReleaseLockAsync(string key, string token);
+        Task<bool> IsLockedAsync(string key);
+    }
+}
---
+++ Danplanner/Danplanner.Application/Models/LockRequest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Danplanner.Application.Models
+{
+    public sealed class LockRequest
+    {
+        public int AccommodationId { get; set; } = 0;
+        public string Start { get; set; } = "";
+        public string End { get; set; } = "";
+        public string PlaceKey { get; set; } = "";
+    }
+}
